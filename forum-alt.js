b89943a236b66ee6de41fa3b38ad1693deffab11
Justin Farrow <landsurveyorsunited@gmail.com>
2018-07-26 14:11:55 -0400

    Add files via upload

---
+++ forum-alt.js
@@ -0,0 +1,63 @@
+if (x$(".xg_widget_forum_category_listByTitle").length > 0) {
+  x$("<img src='http://api.ning.com:80/files/IvR6OvSBmlWUReL-GSmG8e9ifXvcFR1ARVEvienDsrcFUtZkoPo9b2aDy5mPxuO2bEyAohln8vovnAWezTxd0AKkLCeb31xN/icon.png'/>").prependTo("table.byCategory h3");
+  x$('.module_searchbar').css('background', 'black');
+  x$('.xg_lightborder').css('border-color', 'black');
+  x$('table.categories').css('border', 'solid black 2px');
+  x$('table.categories td.bignum').css('border', 'black solid 1px');
+  x$('.xg_span-16').css('width', '982px');
+  x$('.xg_span-5').css('display', 'none');
+  x$('.categories tbody tr:nth-child(odd)').css('background', '#d5d4d4');
+  x$('.categories tbody tr:nth-child(even)').css('background', '#d5d4d4');
+  x$('table.categories th').css({
+    'border': 'red solid 1px',
+    'background': '#212121',
+    'font-size': '18px',
+    'padding': '10px',
+    'font-family': 'arial',
+    'color': 'white'
+  });
+}
+
+
+
+if (x$(".xg_widget_forum_topic_list").length > 0) {
+  x$("<img src='http://api.ning.com:80/files/IvR6OvSBmlWUReL-GSmG8e9ifXvcFR1ARVEvienDsrcFUtZkoPo9b2aDy5mPxuO2bEyAohln8vovnAWezTxd0AKkLCeb31xN/icon.png'/>").prependTo("table.byCategory h3");
+  x$('.module_searchbar').css('background', 'black');
+  x$('.xg_lightborder').css('border-color', 'black');
+  x$('table.categories').css('border', 'solid black 2px');
+  x$('table.categories td.bignum').css('border', 'black solid 1px');
+  x$('.xg_span-16').css('width', '982px');
+  x$('.xg_span-5').css('display', 'none');
+  x$('.categories tbody tr:nth-child(odd)').css('background', '#d5d4d4');
+  x$('.categories tbody tr:nth-child(even)').css('background', '#d5d4d4');
+  x$('table.categories th').css({
+    'border': 'red solid 1px',
+    'background': '#212121',
+    'font-size': '18px',
+    'padding': '10px',
+    'font-family': 'arial',
+    'color': 'white'
+  });
+}
+
+
+
+if (x$(".xg_widget_forum_index_index").length > 0) {
+  x$("<img src='http://api.ning.com:80/files/IvR6OvSBmlWUReL-GSmG8e9ifXvcFR1ARVEvienDsrcFUtZkoPo9b2aDy5mPxuO2bEyAohln8vovnAWezTxd0AKkLCeb31xN/icon.png'/>").prependTo("table.byCategory h3");
+  x$('.module_searchbar').css('background', 'black');
+  x$('.xg_lightborder').css('border-color', 'black');
+  x$('table.categories').css('border', 'solid black 2px');
+  x$('table.categories td.bignum').css('border', 'black solid 1px');
+  x$('.xg_span-16').css('width', '982px');
+  x$('.xg_span-5').css('display', 'none');
+  x$('.categories tbody tr:nth-child(odd)').css('background', '#d5d4d4');
+  x$('.categories tbody tr:nth-child(even)').css('background', '#d5d4d4');
+  x$('table.categories th').css({
+    'border': 'red solid 1px',
+    'background': '#212121',
+    'font-size': '18px',
+    'padding': '10px',
+    'font-family': 'arial',
+    'color': 'white'
+  });
+}
