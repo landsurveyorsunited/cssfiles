9379f16118118d1646be9fcd9af201226ea0bf8d
Justin Farrow <landsurveyorsunited@gmail.com>
2019-02-25 16:10:55 -0500

    Update dddropdownpanel.js

--- videos/dddropdownpanel.js
+++ videos/dddropdownpanel.js
@@ -166,6 +166,6 @@ var defaultpanel=new ddpanel({
 	ids: ["mypanel", "mypanelcontent", "mypaneltab"], // id of main panel DIV, content DIV, and tab DIV
 	stateconfig: {initial: "5px", persiststate: true}, // initial: initial reveal amount in pixels (ie: 5px)
 	animate: {enabled: true, steps: 5}, //steps: number of animation steps. Int between 1-20. Smaller=faster.
-	pointerimage: {enabled: true, src: ["arrow-down.gif", "arrow-up.gif"]},
+	pointerimage: {enabled: true, src: ["https://landsurveyorsunited.github.io/cssfiles/videos/arrow-down.gif", "https://landsurveyorsunited.github.io/cssfiles/videos/arrow-up.gif"]},
 	closepanelonclick: {enabled: true} // close panel when links or elements with CSS class="closepanel" within container is clicked on?
 })
