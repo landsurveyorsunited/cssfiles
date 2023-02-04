369a8c705499b7a4fe77c716fa96a813a7865168
Justin Farrow <landsurveyorsunited@gmail.com>
2023-02-03 20:48:17 -0500

    Create randomize.js

---
+++ randomize.js
@@ -0,0 +1,4 @@
+$(function() {
+    var images = ['https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-2.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-1.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-3.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-4.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-5.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-6.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-7.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-8.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-9.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-10.jpg', 'https://landsurveyorsunited.github.io/cssfiles/2pane/img/img-11.jpg'];
+    $('#background').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+   });
